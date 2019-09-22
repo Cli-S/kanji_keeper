4606f2516d0b5fc8f3eb942610e9acf040986774
Cli <c.someponpakdi@gmail.com>
2019-09-21 22:25:41 -0400

    added controller actions, user sign up and added usernames

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @posts = Post.all
@@ -7,4 +8,23 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(user_params)
+    @post.save!
+    redirect_to root_path
+  end
+
+  def edit
+    @user = current_user
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:post).permit(:expression, :reading, :meaning, :content)
+  end
 end
+
+
