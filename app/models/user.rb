61706e58219a3faef3b1ee984b77806381671de2
Cli <c.someponpakdi@gmail.com>
2019-09-26 22:42:28 -0400

    fixed model for finding favorites of users and pathing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :posts, through: :favorites
+  has_many :posts
   has_many :comments
   has_many :favorites, dependent: :destroy
+  has_many :favorite_posts, through: :posts, source: :favorites
   mount_uploader :photo, PhotoUploader
 end
