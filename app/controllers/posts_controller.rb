1bf00dffb9039038ff8f00c3dd4cd69c63066a74
Cli <c.someponpakdi@gmail.com>
2019-10-01 00:12:57 -0400

    added display order for post index, and fixed uneven comment display issue with html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
