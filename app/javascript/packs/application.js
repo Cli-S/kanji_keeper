2542b2e69a68418f417fa0db849789c0a1d10cef
Cli <c.someponpakdi@gmail.com>
2019-10-01 23:17:35 -0400

    removed turbolinks for the displaying bug that would remove and add furigana on load

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,18 +2,19 @@ import Kuroshiro from "kuroshiro";
 import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji";
 
 const kuroshiro = new Kuroshiro();
-kuroshiro.init(new KuromojiAnalyzer({ dictPath: "/dict" })).then(async () => {
-    const kanjiCards = document.querySelectorAll('.kanji-card');
 
-    kanjiCards.forEach(async (element) => {
+kuroshiro.init(new KuromojiAnalyzer({ dictPath: "/dict" })).then(async () => {
+  const kanjiCards = document.querySelectorAll('.kanji-card');
+
+  kanjiCards.forEach(async (element) => {
     if(element.classList.contains('has-furigana')) {
       return;
     } else {
-    const furiganaKanji = element.querySelector('.furigana-card').innerHTML;
-    const result = await kuroshiro.convert(furiganaKanji, { to: "hiragana", mode: "furigana" });
-    const furiganafy = element.querySelector('.add-furigana');
-    furiganafy.innerHTML = result;
-    element.classList.add('has-furigana');
+      const furiganaKanji = element.querySelector('.furigana-card').innerHTML;
+      const result = await kuroshiro.convert(furiganaKanji, { to: "hiragana", mode: "furigana" });
+      const furiganafy = element.querySelector('.add-furigana');
+      furiganafy.innerHTML = result;
+      element.classList.add('has-furigana');
     }
   })
 });
