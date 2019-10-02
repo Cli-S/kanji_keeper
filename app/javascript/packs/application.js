01e0c95dfdba5db84d3cb615e8069b66be5bf1f1
Cli <c.someponpakdi@gmail.com>
2019-10-01 23:03:13 -0400

    fixed bug with multiplying furigana

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,16 @@ const kuroshiro = new Kuroshiro();
 kuroshiro.init(new KuromojiAnalyzer({ dictPath: "/dict" })).then(async () => {
     const kanjiCards = document.querySelectorAll('.kanji-card');
 
-    kanjiCards.forEach(async (expression) => {
-      const furiganaKanji = expression.querySelector('.furigana-card').innerHTML;
-      const result = await kuroshiro.convert(furiganaKanji, { to: "hiragana", mode: "furigana" });
-      const furiganafy = expression.querySelector('.add-furigana');
-      furiganafy.innerHTML = result;
-    })
+    kanjiCards.forEach(async (element) => {
+    if(element.classList.contains('has-furigana')) {
+      return;
+    } else {
+    const furiganaKanji = element.querySelector('.furigana-card').innerHTML;
+    const result = await kuroshiro.convert(furiganaKanji, { to: "hiragana", mode: "furigana" });
+    const furiganafy = element.querySelector('.add-furigana');
+    furiganafy.innerHTML = result;
+    element.classList.add('has-furigana');
+    }
+  })
 });
+
