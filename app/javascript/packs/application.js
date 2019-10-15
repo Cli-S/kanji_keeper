2e38c3fa1b0a3e63ce61c54af8ee609c441115e3
Cli <c.someponpakdi@gmail.com>
2019-10-15 15:40:10 -0400

    switched json file for api to joyo only, so instead of 13,000 characters it is only the 2000 that are actually in use

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,9 +26,9 @@ kuroshiro.init(new KuromojiAnalyzer({ dictPath: "/dict" })).then(async () => {
   const insertJisho = document.querySelector('.jisho-url');
   const domParser = new DOMParser();
   
-  fetch('https://kanjiapi.dev/v1/kanji/all').then(r => r.json()).then(async (allKanji) => {
+  fetch('https://kanjiapi.dev/v1/kanji/joyo').then(r => r.json()).then(async (allKanji) => {
     const today = new Date;
-    const numberOfTheDay = parseInt(`${today.getDate()}${today.getMonth()}${today.getFullYear()}`);
+    const numberOfTheDay = parseInt(`${today.getDate()+1}${today.getMonth()}${today.getFullYear()}`);
     const randomKanji = allKanji[numberOfTheDay % allKanji.length];
 //  const dailyConversion = await kuroshiro.convert(randomKanji, { to: "hiragana", mode: "furigana" });
 //  const selectedFurigana = domParser.parseFromString(dailyConversion, "text/html").querySelector("rt").innerText;
