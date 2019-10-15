b1e01ee35daf3b641cc06b55c5dbfb5a2b84aa5b
Cli <c.someponpakdi@gmail.com>
2019-10-14 22:57:23 -0400

    removed furigana for daily kanji, added kun and on readings instead

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,25 +20,34 @@ kuroshiro.init(new KuromojiAnalyzer({ dictPath: "/dict" })).then(async () => {
   
   const insertFurigana = document.querySelector('.needs-furi');
   const insertDefinition = document.querySelector('.word-definition');
+  const insertKunOn = document.querySelector('.failed-furi');
   const domParser = new DOMParser();
   
   fetch('https://kanjiapi.dev/v1/kanji/all').then(r => r.json()).then(async (allKanji) => {
     const today = new Date;
-    const numberOfTheDay = parseInt(`${today.getDate()}${today.getMonth()}${today.getFullYear()}`);
+    const numberOfTheDay = parseInt(`${today.getDate()+1}${today.getMonth()}${today.getFullYear()}`);
     const randomKanji = allKanji[numberOfTheDay % allKanji.length];
-    const dailyConversion = await kuroshiro.convert(randomKanji, { to: "hiragana", mode: "furigana" });
-    const selectedFurigana = domParser.parseFromString(dailyConversion, "text/html").querySelector("rt").innerText;
-    const kanjiChecker = Kuroshiro.Util.isHiragana(selectedFurigana);
-    if (kanjiChecker == true) {
-      insertFurigana.innerHTML += dailyConversion;
-    } else {
+//  const dailyConversion = await kuroshiro.convert(randomKanji, { to: "hiragana", mode: "furigana" });
+//  const selectedFurigana = domParser.parseFromString(dailyConversion, "text/html").querySelector("rt").innerText;
+//  const kanjiChecker = Kuroshiro.Util.isHiragana(selectedFurigana);
+//    if (kanjiChecker == true) {
+//      insertFurigana.innerHTML += dailyConversion;
+//    } else {
+//      insertFurigana.innerHTML += randomKanji;
+//    }
+    fetch(`https://kanjiapi.dev/v1/kanji/${randomKanji}`).then(r => r.json()).then((definition) => {
       insertFurigana.innerHTML += randomKanji;
-    }
-    fetch(`https://kanjiapi.dev/v1/words/${randomKanji}`).then(r => r.json()).then((definition) => {
-      console.log(definition)
-      definition.forEach((element) => {
-        const kanjiDef = element['meanings'][0]['glosses'][0];
-        insertDefinition.innerHTML += `<p>${kanjiDef}</p>`;
+      definition['kun_readings'].forEach((element) => {
+        const kunyomi = element;
+        insertKunOn.innerHTML += `<p>Kun readings: ${kunyomi}</p>`;
+      });
+      definition['on_readings'].forEach((element) => {
+        const onyomi = element;
+        insertKunOn.innerHTML += `<p>On readings: ${onyomi}</p>`;
+      });
+      definition['meanings'].forEach((element) => {
+        const kanjiDef = element;
+        insertDefinition.innerHTML += `<li>${kanjiDef}</li>`;
       });
     });
   });
