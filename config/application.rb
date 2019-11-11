7d4a91056b16ace05106db3c133f5fa0f1642138
Cli <c.someponpakdi@gmail.com>
2019-11-11 16:32:34 -0500

    fixed floating font-awesome icons issue with field_with_errors when user has errors with signup form

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module KanjiKeeper
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      html_tag
+    }
   end
 end
