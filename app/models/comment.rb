b875adecc3cbaa8a7869be5f922cf55418ab64d3
Cli <c.someponpakdi@gmail.com>
2020-01-21 00:58:54 -0500

    temporary comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  validates :content, presence: true, length: {minimum: 1}
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat_room
   after_create_commit { MessageBroadcastJob.perform_later self }
-  validates :content, presence: true, length: {minimum: 1, maximum: 1000}
+  validates :content, presence: true, length: {minimum: 1, maximum: 280}
   scope :for_display, -> { order(:created_at).last(5) }
 end
