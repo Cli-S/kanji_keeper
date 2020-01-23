7ab88ca76ecfbed0f4fdb83272ee292a0f8f3e39
Cli <c.someponpakdi@gmail.com>
2020-01-23 00:30:51 -0500

    created message controller, delete messages, fixed comments

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,10 @@
+class MessagesController < ApplicationController
+  def destroy
+    @message = Message.find(params[:id])
+    if @message.user == current_user
+      @message.destroy
+      @chat_room = ChatRoom.find(params[:chat_room_id])
+      redirect_to chat_room_path(@chat_room.id)
+    end
+  end
+end
