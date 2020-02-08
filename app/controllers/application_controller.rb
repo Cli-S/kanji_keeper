f58e35dd7f2427cea9b6d2ce5ce0f363d8464175
Cli <c.someponpakdi@gmail.com>
2020-02-08 13:39:09 -0500

    testing hobby dyno, force ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_user_notifications
-  # before_action :redirect_to_domain
+  before_action :redirect_to_domain
 
   protected
 
@@ -19,11 +19,11 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # def redirect_to_domain
-  #   domain = ENV["DOMAIN"] 
+  def redirect_to_domain
+    domain = ENV["DOMAIN"] 
 
-  #   if domain && request.host == "kanjikeeper.herokuapp.com" || request.host == "kanjikeeper.com"
-  #     redirect_to "http://" + domain + request.path, status: :moved_permanently
-  #   end
-  # end
+  if domain && request.host == "kanjikeeper.herokuapp.com" || request.host == "kanjikeeper.com"
+    redirect_to "http://" + domain + request.path, status: :moved_permanently
+    end
+  end
 end
