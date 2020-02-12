fc8f38f377cc8a31a24d0b837e55d02594a09807
Cli <c.someponpakdi@gmail.com>
2020-02-12 16:07:46 -0500

    testing message delete in production

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,8 +3,9 @@ class MessagesController < ApplicationController
     @message = Message.find(params[:id])
     if @message.user == current_user
       @message.destroy
-      @chat_room = ChatRoom.find(params[:chat_room_id])
-      redirect_to chat_room_path(@chat_room.id)
+      #@chat_room = ChatRoom.find(params[:chat_room_id])
+      redirect_to chat_path
+      #redirect_to chat_room_path(@chat_room.id)
     end
   end
 end
